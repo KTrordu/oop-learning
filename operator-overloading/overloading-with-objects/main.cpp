f7f5c055fa76a09115a85183557ee7aa6c4bde76
KTrordu <alptug.yilmaz@outlook.com>
2025-07-29 05:34:45 +0300

    updated overloading-with-objects

--- operator-overloading/overloading-with-objects/main.cpp
+++ operator-overloading/overloading-with-objects/main.cpp
@@ -6,7 +6,18 @@ public:
 	ComplexNumber(double, double);
 
 	double getSize() const;
+
+	double getRe() const;
+	double getIm() const;
+
+	// overloading the assignment operator is not necessary in this case cuz the default will do the same
+	bool operator==(const ComplexNumber&) const;
 	bool operator>(const ComplexNumber&) const;
+	bool operator>(double) const;
+
+	ComplexNumber operator+(const ComplexNumber&) const;
+
+	void print() const;
 
 private:
 	double m_re{}, m_im{1.0};
@@ -23,9 +34,42 @@ double ComplexNumber::getSize() const
 	return (m_re * m_re + m_im * m_im);
 }
 
+double ComplexNumber::getRe() const
+{
+	return m_re;
+}
+
+double ComplexNumber::getIm() const
+{
+	return m_im;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber& in_number) const
+{
+	return(m_re == in_number.getRe()) && (m_im == in_number.getIm());
+}
+
 bool ComplexNumber::operator>(const ComplexNumber& in_number) const
 {
 	return getSize() > in_number.getSize();
+}
+
+bool ComplexNumber::operator>(double in_number) const
+{
+	return getSize() > in_number;
+}
+
+ComplexNumber ComplexNumber::operator+(const ComplexNumber& in_number) const
+{
+	double res_re = m_re + in_number.getRe();
+	double res_im = m_im + in_number.getIm();
+
+	return ComplexNumber(res_re, res_im);
+}
+
+void ComplexNumber::print() const
+{
+	std::cout << m_re << " + " << m_im << "i" << std::endl;	
 }
 
 int main(int argc, char const *argv[])
@@ -34,6 +78,8 @@ int main(int argc, char const *argv[])
 	ComplexNumber comp2{2.5, 3.7};
 	if(comp1 > comp2) std::cout << "comp1 is greater than comp2" << std::endl;
 	else std::cout << "comp1 is NOT greater than comp2" << std::endl;
+
+	(comp1 + comp2).print();
 
 	return 0;
 }
